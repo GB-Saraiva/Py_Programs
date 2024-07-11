2a0670b4fd45b63c2e1ef6e812a8b473b7c474be
GB-Saraiva <gabrielc.sara@gmail.com>
2024-07-11 01:28:48 -0300

    Sistem logon

--- Jornada_Python/Automação.py
+++ Jornada_Python/Automação.py
@@ -1,18 +1,25 @@
 import pyautogui
+import time
 import os
 
-pyautogui.hotkey('alt', 'tab', interval=0.5)
+os.system('cls')
+
+pyautogui.hotkey('alt', 'tab', interval=1)
 pyautogui.hotkey('ctrl', 't')
 pyautogui.write(
     'https://dlp.hashtagtreinamentos.com/python/intensivao/login', interval=0.1)
-pyautogui.press('enter')
+pyautogui.press('enter', interval=10)
+pyautogui.click(x=653, y=394)
+pyautogui.hotkey('ctrl', 'a')
+pyautogui.write('seuemailaqui@gmail.com', interval=0.1)
+pyautogui.press('tab', interval=1)
+pyautogui.write('sua_senh@AQUI', interval=0.1)
+time.sleep(2)
+pyautogui.click(x=675, y=557)
 
 
 
-
-
-
-
+os.system('pause')
 os.system('cls')
 # Pra abrir o chrome:
 # pyautogui.press('win')
--- Jornada_Python/clicar.py
+++ Jornada_Python/clicar.py
@@ -1,1 +1,12 @@
-print('200-147')
+import time
+import pyautogui
+import os
+
+os.system('cls')
+
+print('Posição do mouse: ', end='')
+time.sleep(5)
+print(pyautogui.position())
+
+os.system('pause')
+os.system('cls')
